cc78ccad8047f02d6180c43b161554840aa54865
F4Tornado school compy <h.rovnyak@gmail.com>
2020-03-06 15:51:10 -0500

    Markov order and mutiple word generations

--- sketch.js
+++ sketch.js
@@ -5,8 +5,53 @@ let charsObj = {
 };
 let start = [];
 let word = [];
+let order = 2;
 
 function generate() {
+    let words = [];
+    let number = parseInt(document.getElementById("amt").value);
+
+    // Loop for every word the user wants
+    for (let i = 0; i < number; i++) {
+        word = [];
+
+        // Generate a random start to a word, unless one is provided
+        let startIn = document.getElementById("start").value;
+        if (startIn && startIn.length >= order) {
+            word = startIn.split("");
+        } else {
+            word = start[Math.floor(start.length * Math.random())].split("");
+        }
+
+        // Add letters until coming upon a " "
+        while (word[word.length - 1] !== " ") {
+            word.push(charChances[word.slice(word.length - order, word.length).join("")][Math.floor(Math.random() * charChances[word.slice(word.length - order, word.length).join("")].length)]);
+        }
+
+        // Remove the excess " ", convert to string, and add to words array
+        word.pop();
+        words.push(word.join(""));
+    }
+
+    document.getElementById("words").innerHTML = words.join(", ");
+}
+
+window.onload = () => {
+    if (localStorage.getItem("corpus")) {
+        document.getElementById("corpus").value = localStorage.getItem("corpus");
+    }
+    document.getElementById("corpus").onchange = () => {
+        localStorage.setItem("corpus", document.getElementById("corpus").value);
+        calculateChances();
+    }
+    document.getElementById("order").onchange = () => {
+        order = parseInt(document.getElementById("order").value);
+        calculateChances();
+    }
+    calculateChances();
+}
+
+function calculateChances() {
     // Reset variables
     charChances = {};
     chars = [];
@@ -14,15 +59,15 @@ function generate() {
         " ": 0
     };
     start = [];
-    let word = [];
 
     // Get the list of characters the user is using & initialize the variables
     let corpus = document.getElementById("corpus").value.split(" ");
     for (let i = 0; i < corpus.length; i++) {
         for (let j = 0; j < corpus[i].length; j++) {
-            if (chars.indexOf(corpus[i][j]) == -1) {
-                chars.push(corpus[i][j]);
-                charsObj[corpus[i][j]] = 0;
+            // console.log(corpus[i].slice(j, j + order).length);
+            if (corpus[i].slice(j, j + order).length == order && chars.indexOf(corpus[i].slice(j, j + order)) == -1) {
+                chars.push(corpus[i].slice(j, j + order));
+                charsObj[corpus[i].slice(j, j + order)] = 0;
             }
         }
     }
@@ -32,9 +77,9 @@ function generate() {
         charChances[chars[i]] = [];
         for (let j = 0; j < corpus.length; j++) {
             for (let k = 0; k < corpus[j].length; k++) {
-                if (chars[i] == corpus[j][k]) {
-                    if (corpus[j][k + 1]) {
-                        charChances[chars[i]].push(corpus[j][k + 1]);
+                if (chars[i] == corpus[j].slice(k, k + order)) {
+                    if (corpus[j][k + order]) {
+                        charChances[chars[i]].push(corpus[j][k + order]);
                     } else {
                         charChances[chars[i]].push(" ");
                     }
@@ -46,32 +91,6 @@ function generate() {
     // Calculate the chance a letter starts a word
     start = [];
     for (let i = 0; i < corpus.length; i++) {
-        start.push(corpus[i][0]);
-    }
-
-    // Generate a random start to a word, unless on is provided
-    let startIn = document.getElementById("start").value;
-    if (startIn) {
-        word = startIn.split("");
-    } else {
-        word.push(start[Math.floor(start.length * Math.random())]);
-    }
-
-    // Add letters until coming upon a " "
-    while (word[word.length - 1] !== " ") {
-        word.push(charChances[word[word.length - 1]][Math.floor(Math.random() * charChances[word[word.length - 1]].length)]);
-    }
-
-    // Remove the excess " ",convert to string, and add put into a <span>
-    word.pop();
-    document.getElementById("words").innerHTML = word.join("");
-}
-
-window.onload = () => {
-    if (localStorage.getItem("corpus")) {
-        document.getElementById("corpus").value = localStorage.getItem("corpus");
-    }
-    document.getElementById("corpus").onchange = () => {
-        localStorage.setItem("corpus", document.getElementById("corpus").value)
+        start.push(corpus[i].slice(0, order));
     }
 }
